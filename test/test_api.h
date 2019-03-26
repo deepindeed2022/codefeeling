31f78649611e8b00160b87ecd87575d6c6f12309
caowenlong <caowenlong@sensetime.com>
2019-03-26 22:16:49 +0800

    update full path

--- test/test_api.h
+++ test/test_api.h
@@ -4,14 +4,24 @@
 #include <util.h>
 #include <time_helper.h>
 #include <vector>
+
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <cstdlib>
+#endif
 int get_int_rand();
 std::vector<int> generate_int_array(const size_t size);
 inline std::string test_path(const std::string& name) {
-#ifdef _MSC_VER
-    return std::string("D:\\Repo\\IntroductionToAlgorithm\\test\\") + name;
+#define PATH_MAX 128
+    char acCanonicalPath[PATH_MAX];
+    memset(acCanonicalPath, 0, PATH_MAX);
+#ifdef _WIN32
+    _fullpath(acCanonicalPath, "./", PATH_MAX);
 #else
-    return std::string("/home/sensetime/Repo/IntroductionToAlgorithm/test/") + name;
+    realpath("./", acCanonicalPath, PATH_MAX);
 #endif
+    return std::string(acCanonicalPath) + name;
 }
 template <typename T>
 errcode_t printArr(T *arr, const size_t size)
