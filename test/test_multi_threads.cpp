037711a6dbdc3ce357e1c8a0ec7a217d1e772af9
caowenlong <caowenlong@sensetime.com>
2019-05-31 17:43:30 +0800

    add multi thread testcase

---
+++ test/test_multi_threads.cpp
@@ -0,0 +1,26 @@
+#include <gtest/gtest.h>
+#include <thread>
+#include <mutex>
+int kMultiCount = 0;
+std::mutex mt;
+
+void func(const int id, const int k) {
+	
+	int s = 0;
+	
+	// mt.lock();
+	for(int i = 1; i <= k; ++i)
+		s += i;
+	std::cout << "total£¨" << id <<")="<<s<< std::endl;
+	// mt.unlock();
+	std::cout<< "thread " << ++kMultiCount << " finished\n";
+}
+
+TEST(MultiThreads, common)
+{
+	std::vector<std::thread> threads;
+	for (int i = 0; i < 10; i++) threads.emplace_back(func, i, 2);
+	for(int i = 0; i < 10; i++)
+		threads[i].join();
+
+}
