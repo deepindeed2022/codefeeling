bf71a0212bca69aebdd5cb2fdd565ec074a429c5
caowenlong <caowenlong92@gmail.com>
2019-09-10 00:53:18 +0800

    add nms

--- include/dl/extend_op.h
+++ include/dl/extend_op.h
@@ -1,5 +1,27 @@
 #ifndef INCLUDE_DL_EXTEND_OP_H_
 #define INCLUDE_DL_EXTEND_OP_H_
-// todo: NMS
+#include <cf_util.h>
+#include <vector>
+typedef struct cf_point_t {
+    int x;
+    int y;
+} cf_point_t;
 
+static cf_rect_t combine_box(cf_point_t& lt, cf_point_t& rb) {
+    return cf_rect_t{lt.x, lt.y, rb.x, rb.y};
+}
+static float InterSectionArea(cf_rect_t& a, cf_rect_t& b) {
+    int l = std::max(a.left, b.left);
+    int t = std::max(a.top, b.top);
+    int r = std::min(a.right, b.right);
+    int b = std::min(a.bottom, b.bottom);
+}
+typedef struct BBox {
+    cf_rect_t position;
+    float score;
+} BBox;
+
+std::vector<BBox> nonMaximumSuppression(std::vector<BBox>& bbox, float overlapThreshold) {
+
+}
 #endif
