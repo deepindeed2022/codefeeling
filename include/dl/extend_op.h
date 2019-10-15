812f938805a886714d97685d1991d2935f5db75e
caowenlong <caowenlong92@gmail.com>
2019-10-15 14:45:32 +0800

    fixbug nms libs

--- include/dl/extend_op.h
+++ include/dl/extend_op.h
@@ -3,6 +3,7 @@
 #include <cf_util.h>
 #include <vector>
 #include <algorithm>
+#include <iostream>
 static int InterSectionArea(cf_rect_t& aa, cf_rect_t& bb) {
 	int l = std::max(aa.left, bb.left);
 	int t = std::max(aa.top, bb.top);
@@ -23,14 +24,14 @@ std::vector<BBox> nonMaximumSuppression(std::vector<BBox>& bbox, float overlapTh
 	std::vector<float> bbox_area(bbox_size, 0);
 	for(int i = 0; i < bbox_size; i++) {
 		cf_rect_t rect = bbox[i].position;
-		bbox_area[i] = (rect.right - rect.right + 1)*(rect.bottom - rect.top + 1);
+		bbox_area[i] = (rect.right - rect.left + 1)*(rect.bottom - rect.top + 1);
 	}
 	for (int i = 0; i < bbox_size; i++) {  
 		if (is_suppressed[i]) continue;
 		for (int j = i + 1; j < bbox_size; j++) {
 			if (is_suppressed[j]) continue;
 			int overlap = InterSectionArea(bbox[i].position, bbox[j].position);
-			if (overlap > 0) {  
+			if (overlap > 0) {
 				float overlapRatio = overlap / (bbox_area[i] + bbox_area[j] - overlap);	// 计算重叠的比率
 				if (overlapRatio > overlapThreshold) {
 					is_suppressed[j] = true; 		// 将窗口j标记为抑制
--- test/dl/test_extend_op.cpp
+++ test/dl/test_extend_op.cpp
@@ -3,12 +3,10 @@
 #include "test_api.h"
 
 TEST(DLExtendOp, NMS) {
-    BBox a = {{1, 1, 9, 9}, 0.9};
-    BBox b = {{0, 0, 4, 4}, 0.8};
-    BBox c = {{7, 7, 8, 8}, 0.9};
-    BBox d = {{1, 1, 2, 2}, 0.76};
-    std::vector<BBox> input = {a, b, c, d};
-    std::vector<BBox> result = nonMaximumSuppression(input, 0.5);
+    BBox a = {{168,127,363,353}, 0.87};
+    BBox b = {{169,114,361,369}, 0.546};
+    std::vector<BBox> input = {a, b};
+    std::vector<BBox> result = nonMaximumSuppression(input, 0.3);
     for(auto& r: result) {
         cf_rect_t rect = r.position;
         fprintf(stderr, "{{%d, %d, %d, %d}, %f}\n", rect.left, rect.top, rect.right, rect.bottom, r.score);
