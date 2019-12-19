aba295a5c1c3937979f63db049b4c30a6d76d453
caowenlong <caowenlong92@gmail.com>
2019-12-19 15:05:43 +0800

    add cmake creating packages samples

---
+++ cmaketest/climbingstats.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "climbingstats.h"
+
+int climbing(int a) {
+    std::cout << "climbing " << a << std::endl;
+    return a*a;
+}
---
+++ cmaketest/climbingstats.h
@@ -0,0 +1,4 @@
+#include <iostream>
+#include <algorithm>
+
+int climbing(int a);
